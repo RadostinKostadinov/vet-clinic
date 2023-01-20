85155a683ac8f5c9b2242e6edb72b3444d89f991
Radostin Kostadinov <bg.radostin.kostadinov@gmail.com>
2023-01-20 17:09:13 +0200

    Adds possibilty to connect to SQL DB

--- RestAPI/app.js
+++ RestAPI/app.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import sql from 'mssql';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -13,9 +14,17 @@ app.use(cors());
 app.use('/static', express.static('./static'));
 
 app.get('/', (req, res) => {
-  res.send('VET CLINIC API - working');
+  res.send('VET CLINIC API - working.');
 });
 
 app.listen(PORT, () => {
-  console.log('SUCCESS: VET CLINIC API - started');
+  console.log('SUCCESS: VET CLINIC API - started.');
+});
+
+sql.connect(`Driver={ODBC Driver 18 for SQL Server};Server=tcp:rk-recheck-vet-clinic.database.windows.net,1433;Database=${process.env.DATABASE};Uid=${process.env.DB_UID};Pwd=${process.env.DB_PASSWORD};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;`, (err) => {
+  if (err) {
+    throw new Error('Can\'t connect to DB.');
+  } else {
+    console.log('SUCCESS: VET CLINIC API - connected to DB.');
+  }
 });
